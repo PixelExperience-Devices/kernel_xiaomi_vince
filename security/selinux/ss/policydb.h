57e0fc3af4a31ca1dce44d8a33058e40401575b6
Rapherion Rollerscaperers <rapherion@raphielgang.org>
2020-03-16 21:59:44 +0700

    [REVERT 1/2] "ANDROID: selinux: modify RTM_GETLINK permission"
    This commit causing a page handling failure if the kernel
    compiled with LLD, and as the commit message says, this is
    just a temporary Android-only patch that will deprecated in
    newer kernel, new fix will be backported after it arrives
    on upstream (or if Google backported that before I do)
    
    This reverts commit 2c434f622e5075e30b62829fa25dc36f56141594.
    
    Bug: SELinux Breaks in LLD
    
    Signed-off-by: Rapherion Rollerscaperers <rapherion@raphielgang.org>
    Signed-off-by: Twisted <36546624+TwistedPrime@users.noreply.github.com>
    Signed-off-by: MadeOfGreat <ravenklawasd@gmail.com>
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>
    Signed-off-by: rk134 <rk134@pixelexperience.org>

--- security/selinux/ss/policydb.h
+++ security/selinux/ss/policydb.h
@@ -227,7 +227,6 @@ struct genfs {
 /* The policy database */
 struct policydb {
 	int mls_enabled;
-	int android_netlink_route;
 	int android_netlink_getneigh;
 
 	/* symbol tables */
@@ -315,7 +314,6 @@ extern int policydb_write(struct policydb *p, void *fp);
 #define PERM_SYMTAB_SIZE 32
 
 #define POLICYDB_CONFIG_MLS    1
-#define POLICYDB_CONFIG_ANDROID_NETLINK_ROUTE    (1 << 31)
 #define POLICYDB_CONFIG_ANDROID_NETLINK_GETNEIGH (1 << 30)
 
 /* the config flags related to unknown classes/perms are bits 2 and 3 */
--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -70,7 +70,6 @@
 #include "ebitmap.h"
 #include "audit.h"
 
-int selinux_android_netlink_route;
 int selinux_android_netlink_getneigh;
 int selinux_policycap_netpeer;
 int selinux_policycap_openperm;
@@ -1992,8 +1991,6 @@ static void security_load_policycaps(void)
 						  POLICYDB_CAPABILITY_OPENPERM);
 	selinux_policycap_alwaysnetwork = ebitmap_get_bit(&policydb.policycaps,
 						  POLICYDB_CAPABILITY_ALWAYSNETWORK);
-
-	selinux_android_netlink_route = policydb.android_netlink_route;
 	selinux_android_netlink_getneigh = policydb.android_netlink_getneigh;
 	selinux_nlmsg_init();
 }
