a1ed37259e8dcbefc0f3f6936b6d80eb3616e6bd
Mathias Krause <minipli@grsecurity.net>
2021-07-06 15:27:13 +0200

    wireguard: compat: account for grsecurity backports and changes
    grsecurity kernels tend to carry additional backports and changes, like
    commit b60b87fc2996 ("netlink: add ethernet address policy types") or
    the SYM_FUNC_* changes. RAP nowadays hooks the latter, therefore no
    diversion to RAP_ENTRY is needed any more.
    
    Instead of relying on the kernel version test, also test for the macros
    we're about to define to not already be defined to account for these
    additional changes in the grsecurity patch without breaking
    compatibility to the older public ones.
    
    Also test for CONFIG_PAX instead of RAP_PLUGIN for the timer API related
    changes as these don't depend on the RAP plugin to be enabled but just a
    PaX/grsecurity patch to be applied. While there is no preprocessor knob
    for the latter, use CONFIG_PAX as this will likely be enabled in every
    kernel that uses the patch.
    
    Signed-off-by: Mathias Krause <minipli@grsecurity.net>
    [zx2c4: small changes to include a header nearby a macro def test]
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>
    Signed-off-by: rk134 <rk134@pixelexperience.org>

--- net/wireguard/compat/compat-asm.h
+++ net/wireguard/compat/compat-asm.h
@@ -51,7 +51,7 @@
 #undef pull
 #endif
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 4, 76) && !defined(ISRHEL8) && !defined(SYM_FUNC_START)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 4, 76) && !defined(ISCENTOS8S) && !defined(SYM_FUNC_START)
 #define SYM_FUNC_START ENTRY
 #define SYM_FUNC_END ENDPROC
 #endif
